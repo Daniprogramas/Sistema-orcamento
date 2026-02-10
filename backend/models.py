ae291101ca1671395a3ee2aac35f190b811136ad
Danielle Morais <111515555+Daniprogramas@users.noreply.github.com>
2026-02-10 16:45:32 -0300

    Delete backend/models.py

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String, Float
-from database import Base
-
-class Orcamento(Base):
-    __tablename__ = "orcamentos"
-    id = Column(Integer, primary_key=True, index=True)
-    cliente = Column(String, index=True)
-    servico = Column(String)
-    valor = Column(Float)
