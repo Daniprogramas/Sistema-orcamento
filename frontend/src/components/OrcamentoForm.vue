d31a2dd516cc7f6669127b82ba071a16d4aa5f59
Danielle Morais <111515555+Daniprogramas@users.noreply.github.com>
2026-02-10 16:40:59 -0300

    Add OrcamentoForm component for budget input

---
+++ frontend/src/components/OrcamentoForm.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <input v-model="descricao" placeholder="Descrição" />
+    <input v-model.number="valor" type="number" placeholder="Valor" />
+    <button @click="salvar">Salvar</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { api } from '../services/api'
+
+const descricao = ref('')
+const valor = ref(0)
+
+const salvar = async () => {
+  await api.post('/orcamentos', {
+    descricao: descricao.value,
+    valor: valor.value
+  })
+  alert('Orçamento salvo!')
+}
+</script>
